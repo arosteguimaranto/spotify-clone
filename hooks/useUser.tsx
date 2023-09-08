d41a00c8f563582d33933beaa13945557361242b
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-09-07 22:04:01 -0600

    feat: finish providers for auth

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -4,7 +4,8 @@ import {
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
 import { UserDetails, Subscription } from "@/types";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
 
 type UserContextType = {
   accessToken: string | null;
@@ -79,3 +80,12 @@ export const MyUserContextProvider = (props: Props) => {
 
   return <UserContext.Provider value={value} {...props} />;
 };
+
+export const useUser = () =>{
+  const context = useContext(UserContext);
+  if(context === undefined){
+    throw new Error('useUser must be used within a MyUserContextProvider');
+  }
+
+  return context;
+}
