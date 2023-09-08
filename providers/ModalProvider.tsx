e9e6b35aa70c0db125b8d53ac153525ff60381de
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-09-08 12:08:45 -0600

    feat: add Modal Provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+      setIsMounted(true);
+    
+    }, []);
+
+    if(!isMounted){
+        return null;
+    }
+
+
+
+    
+  return (
+    <div>
+      <>
+      Modals
+      </>
+    </div>
+  )
+}
+
+export default ModalProvider
