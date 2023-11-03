1d95b0c8740afcde540f98c3e5b3cfbc2a0be9dc
arosteguimaranto <arosteguimaranto@outlook.com>
2023-11-03 15:42:37 -0600

    feat: add Subscribe modal and account page

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import AuthModal from "@/components/AuthModal";
-import UploadModal from "@/components/UploadModal";
-
 import { useEffect, useState } from "react";
 
-const ModalProvider = () => {
+import AuthModal from "@/components/AuthModal";
+import SubscribeModal from "@/components/SubscribeModal";
+import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
+
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({
+  products
+}) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -18,10 +26,11 @@ const ModalProvider = () => {
 
   return (
     <>
-     <AuthModal/>
-     <UploadModal/>
+      <AuthModal />
+      <SubscribeModal products={products} />
+      <UploadModal />
     </>
   );
-};
+}
 
 export default ModalProvider;
